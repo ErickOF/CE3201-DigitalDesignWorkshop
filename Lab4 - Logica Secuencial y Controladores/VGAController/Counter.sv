3744a013f205a8a3a7684c602bf509bde1f0cece
Kisung Lim Ogawa <kisungqueso@hotmail.com>
2019-10-15 08:09:37 -0600

    Update with some changes in VGA Controller.
    Still not working.

--- Lab4 - Logica Secuencial y Controladores/VGAController/Counter.sv
+++ Lab4 - Logica Secuencial y Controladores/VGAController/Counter.sv
@@ -1,7 +1,7 @@
 module Counter (input clk, reset,
                 output logic [9:0] Q);
 
-always @(posedge clk or posedge reset)
+always_ff @(posedge clk, posedge reset)
 begin
     if (reset) Q <= 9'b0;
     else Q <= Q + 9'b1;
--- Lab4 - Logica Secuencial y Controladores/VGAController/VGAController.sv
+++ Lab4 - Logica Secuencial y Controladores/VGAController/VGAController.sv
@@ -3,26 +3,31 @@ module VGAController(input clk,
                      output logic [9:0] posx, posy);
 
 // VGA constants
-const logic [9:0] H_SYNC_A = 10'd95 - 10'b1;
+const logic [9:0] H_SYNC_A = 10'd95;
 const logic [9:0] H_BACK_PORCH_B = 10'd48;
 const logic [9:0] H_DISPLAY_C = 10'd635;
 const logic [9:0] H_FRONT_PORCH_D = 10'd15;
+//const logic [9:0] H_SYNC_STR = H_FRONT_PORCH_D;
+//const logic [9:0] H_SYNC_END = H_SYNC_A + H_FRONT_PORCH_D;
 const logic [9:0] H_TOTAL = H_SYNC_A + H_BACK_PORCH_B + H_DISPLAY_C + H_FRONT_PORCH_D;
 // const [9:0] H_START = H_SYNC_A + H_BACK_PORCH_B;
 // const [9:0] H_END   = H_SYNC_A + H_BACK_PORCH_B + H_DISPLAY_C - 10'b1;
 
-const logic [9:0] V_SYNC_A = 10'd2 - 10'b1;
+const logic [9:0] V_SYNC_A = 10'd2;
 const logic [9:0] V_UP = 10'd33;
 const logic [9:0] V_DISPLAY = 10'd480;
 const logic [9:0] V_DOWN = 10'd10;
+//const logic [9:0] V_SYNC_STR = V_DISPLAY + V_DOWN;
+//const logic [9:0] V_SYNC_END = V_DISPLAY + V_DOWN + V_SYNC_A;
 const logic [9:0] V_TOTAL = V_SYNC_A + V_UP + V_DISPLAY + V_DOWN;
 // const [9:0] V_START = V_SYNC_A + V_UP - 10'b1;
 // const [9:0] V_END   = V_SYNC_A + V_UP + V_DISPLAY - 10'b1;
 
 // Vars
 logic rsth, rstv;
-logic lower, greater, equal;
+//logic lower, greater, equal;
 logic h_sync_temp1, v_sync_temp1;
+//		h_sync_temp2, v_sync_temp2;
 
 
 Counter h_counter(clk, rsth, posx);
@@ -35,17 +40,19 @@ begin
     #10 rsth = 0; rstv = 0;
 end
 
-Comparator hmin_comparator(posx, H_SYNC_A, h_sync_temp1, greater, equal);
-// Comparator hmax_comparator(posx, H_END, h_sync_temp2, greater, equal);
-Comparator maxscreenh_comparator(posx, H_TOTAL, lower, greater, rsth); 
+Comparator hmin_comparator(.signal1(posx), .signal2(H_SYNC_A), .lower(h_sync_temp1), .greater(), .equal());
+//Comparator hmin_comparator(posx, H_SYNC_STR, lower, h_sync_temp1, equal);
+//Comparator hmax_comparator(posx, H_SYNC_END, h_sync_temp2, greater, equal);
+Comparator maxscreenh_comparator(.signal1(posx), .signal2(H_TOTAL), .lower(), .greater(), .equal(rsth)); 
 
-Comparator vmin_comparator(posy, V_SYNC_A, v_sync_temp1, greater, equal);
-// Comparator vmax_comparator(posy, V_END, v_sync_temp2, greater, equal);
-Comparator maxscreenv_comparator(posy, V_TOTAL, lower, greater, rstv); 
+Comparator vmin_comparator(.signal1(posy), .signal2(V_SYNC_A), .lower(v_sync_temp1), .greater(), .equal());
+//Comparator vmax_comparator(posy, V_SYNC_END, v_sync_temp2, greater, equal);
+Comparator maxscreenv_comparator(.signal1(posy), .signal2(V_TOTAL), .lower(), .greater(), .equal(rstv)); 
 
-assign h_sync = ~h_sync_temp1;
-assign v_sync = ~v_sync_temp1;
+assign h_sync = ~(h_sync_temp1); //& h_sync_temp2);
+assign v_sync = ~(v_sync_temp1); //& v_sync_temp2);
 assign sync_n = h_sync ^ v_sync;
-assign blank_n = h_sync & v_sync;
+assign blank_n = (posx < H_SYNC_A + H_BACK_PORCH_B) | (posx > H_FRONT_PORCH_D) | 
+						(posy < V_SYNC_A + V_UP) | (posy > V_DOWN);
 
 endmodule // VGAController
