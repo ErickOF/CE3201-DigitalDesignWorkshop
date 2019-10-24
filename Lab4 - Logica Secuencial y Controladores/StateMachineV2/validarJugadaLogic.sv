0319f060997e632b120f1fce787ab3d3f9f43365
Maryxd <mary1300@live.com>
2019-10-23 20:29:15 -0600

    Update validarJugadaLogic.sv

--- Lab4 - Logica Secuencial y Controladores/StateMachineV2/validarJugadaLogic.sv
+++ Lab4 - Logica Secuencial y Controladores/StateMachineV2/validarJugadaLogic.sv
@@ -10,15 +10,8 @@ logic [1:0] pos1, pos2, pos3, pos4, pos5, pos6, pos7, pos8, pos9; //Boxes with I
 logic [3:0] current_state;
 logic en1, en2, en3, en4, en5, en6, en7, en8, en9;
 
-regPos box1(en1, clock, reset, playerID, pos1);
-regPos box2(en2, clock, reset, playerID, pos2);
-regPos box3(en3, clock, reset, playerID, pos3);
-regPos box4(en4, clock, reset, playerID, pos4);
-regPos box5(en5, clock, reset, playerID, pos5);
-regPos box6(en6, clock, reset, playerID, pos6);
-regPos box7(en7, clock, reset, playerID, pos7);
-regPos box8(en8, clock, reset, playerID, pos8);
-regPos box9(en9, clock, reset, playerID, pos9);
+
+registroPos(clock, reset, playerID, en1, en2, en3, en4, en5, en6, en7, en8, en9, pos1, pos2, pos3, pos4, pos5, pos6, pos7, pos8, pos9);
 
 
 // current state registers 
